169ccad0e7bd0110a16fb88bc30e5f9d7c1f7bda
winghimjns <winghim_01@hotmail.com>
2022-12-16 21:26:47 +0800

    feat(base): built up the base of application

---
+++ components/NavBar.tsx
@@ -0,0 +1,21 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+
+export default function NavBar() {
+    return (
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" component="div">
+              {/* TODO use config for display name */}
+              BMI Visualiser
+            </Typography>
+          </Toolbar>
+        </AppBar>
+    )
+}
