d7819c48e38a63eccadf4faedd9ecc6c77e09650
winghimjns <winghim_01@hotmail.com>
2022-12-16 22:42:54 +0800

    style(components/navbar.tsx): trimmed indentation

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,17 +5,17 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 
 export default function NavBar() {
-    return (
-        <AppBar position="static">
-          <Toolbar variant="dense">
-            <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" component="div">
-              {/* TODO use config for display name */}
-              BMI Visualiser
-            </Typography>
-          </Toolbar>
-        </AppBar>
-    )
+  return (
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" component="div">
+          {/* TODO use config for display name */}
+          BMI Visualiser
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
 }
