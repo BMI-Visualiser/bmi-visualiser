782e0e842ee445ae5b750180a547f39aa8a9fc62
winghimjns <winghim_01@hotmail.com>
2022-12-16 22:59:18 +0800

    feat(components/navbar.tsx): use h1 tag for page title

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,7 +11,7 @@ export default function NavBar() {
         <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" color="inherit" component="div">
+        <Typography variant="h6" color="inherit" component="h1">
           {/* TODO use config for display name */}
           BMI Visualiser
         </Typography>
